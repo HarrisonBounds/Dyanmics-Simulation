3c2fdc7bbcd86a0acf5cbb9539fc96d5b6d81876
Harrison Bounds <harrison.bounds777@gmail.com>
2024-12-05 10:26:49 -0600

    Intial setup

---
+++ solve.py
@@ -0,0 +1,23 @@
+import sympy
+
+class Solver():
+    def __init__(self):
+        pass
+    def unhat(self):
+        pass
+    def inv(self):
+        pass
+    def find_transforms(self):
+        pass
+    def EL_equations(self):
+        pass
+    def simulate(self):
+        pass
+    def integrate(self):
+        pass
+    def xddot(self):
+        pass
+    def dyn(self, s):
+        pass
+        
+    
