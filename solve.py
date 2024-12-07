2454b90fc8d6d3204cc1b6f4a835eb5bc7c0ef47
Harrison Bounds <harrison.bounds777@gmail.com>
2024-12-07 15:12:39 -0600

    Initial frames setup

--- solve.py
+++ solve.py
@@ -1,7 +1,50 @@
-import sympy
+import sympy as sym
 
 class Solver():
     def __init__(self):
+        self.g, self.m1, self.m2, self.m3, self.m4, self.m_box, self.t = sym.symbols('L1 L2 g m1 m2 m3 m4 mbox t x y')
+
+        self.x_box = sym.Function('x_box')(self.t)
+        self.y_box = sym.Function('y_box')(self.t)
+        self.x_jack = sym.Function('x_jack')(self.t)
+        self.y_jack = sym.Function('y_jack')(self.t)
+        self.theta_box = sym.Function('theta_box')(self.t)
+        self.theta_jack = sym.Function('theta_jack')(self.t)
+        
+        self.inertia_matrix_box = sym.Matrix([[self.m_box, 0, 0, 0, 0, 0],
+                                         [0, self.m_box, 0, 0, 0, 0],
+                                         [0, 0, self.m_box, 0, 0, 0],
+                                         [0, 0, 0, 0, 0, 0],
+                                         [0, 0, 0, 0, 0, 0],
+                                         [0, 0, 0, 0, 0, 1]])
+        
+        self.q = sym.Matrix([self.x_box, self.y_box, self.theta_box, self.x_jack, self.y_jack, self.theta_jack])
+        self.qdot = self.q.diff(self.t)
+        self.qddot = self.qdot.diff(self.t)
+        
+        self.g_w_box_rotation = sym.Matrix([[sym.cos(self.theta_box), -sym.sin(self.theta_box), 0, 0],
+                                       [sym.sin(self.theta_box), sym.cos(self.theta_box), 0, 0],
+                                       [0, 0, 1, 0],
+                                       [0, 0, 0, 1]])
+        
+        self.g_w_box_translation = sym.Matrix([[1, 0, 0, self.x_box],
+                                          [0, 1, 0, self.y_box],
+                                          [0, 0, 1, 0],
+                                          [0, 0, 0, 1]])
+        
+        self.g_box_jack_rotation = sym.Matrix([[sym.cos(self.theta_jack), -sym.sin(self.theta_jack), 0, 0],
+                                          [sym.sin(self.theta_jack), sym.cos(self.theta_jack), 0, 0],
+                                          [0, 0, 1, 0],
+                                          [0, 0, 0, 1]])
+        
+        self.g_box_jack_translation = sym.Matrix([[1, 0, 0, self.x_jack],
+                                             [0, 1, 0, self.y_jack],
+                                             [0, 0, 1, 0],
+                                             [0, 0, 0, 1]])
+        
+        self.g_w_box = self.g_w_box_rotation @ self.g_box_jack_translation
+        self.g_w_jack = self.g_w_box @ (self.g_box_jack_rotation @ self.g_box_jack_translation)
+    def compute_Lagrangian(self):
         pass
     def unhat(self):
         pass
@@ -19,5 +62,6 @@ class Solver():
         pass
     def dyn(self, s):
         pass
+    
         
     
