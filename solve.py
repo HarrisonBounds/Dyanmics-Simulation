387f9ba500832401f361212880e90cab7a154012
Harrison Bounds <harrison.bounds777@gmail.com>
2024-12-07 15:32:27 -0600

    Calulcated the lagrangian of the jack

--- solve.py
+++ solve.py
@@ -2,7 +2,7 @@ import sympy as sym
 
 class Solver():
     def __init__(self):
-        self.g, self.m1, self.m2, self.m3, self.m4, self.m_box, self.t = sym.symbols('L1 L2 g m1 m2 m3 m4 mbox t x y')
+        self.g, self.m1, self.m2, self.m3, self.m4, self.m_box, self.t, self.r = sym.symbols('g m1 m2 m3 m4 mbox t x y r')
 
         self.x_box = sym.Function('x_box')(self.t)
         self.y_box = sym.Function('y_box')(self.t)
@@ -10,6 +10,8 @@ class Solver():
         self.y_jack = sym.Function('y_jack')(self.t)
         self.theta_box = sym.Function('theta_box')(self.t)
         self.theta_jack = sym.Function('theta_jack')(self.t)
+        
+        self.y_matrix = sym.Matrix([0, 1, 0, 0])
         
         self.inertia_matrix_box = sym.Matrix([[self.m_box, 0, 0, 0, 0, 0],
                                          [0, self.m_box, 0, 0, 0, 0],
@@ -44,8 +46,25 @@ class Solver():
         
         self.g_w_box = self.g_w_box_rotation @ self.g_box_jack_translation
         self.g_w_jack = self.g_w_box @ (self.g_box_jack_rotation @ self.g_box_jack_translation)
+        
     def compute_Lagrangian(self):
-        pass
+        ke_r1 = 1/2 * self.m1 * self.g_w_jack @ (self.g_w_jack @ sym.Matrix(self.r, 0, 0, 1))
+        ke_r2 = 1/2 * self.m2 * self.g_w_jack @ (self.g_w_jack @ sym.Matrix(0, -self.r, 0, 1))
+        ke_r3 = 1/2 * self.m3 * self.g_w_jack @ (self.g_w_jack @ sym.Matrix(-self.x, 0, 0, 1))
+        ke_r4 = 1/2 * self.m4 * self.g_w_jack @ (self.g_w_jack @ sym.Matrix(0, self.y, 0, 1))
+        
+        pe_r1 = self.m1 * self.g * self.y_matrix @ (self.g_w_jack @ sym.Matrix(self.r, 0, 0, 1))
+        pe_r2 = self.m2 * self.g * self.y_matrix @ (self.g_w_jack @ sym.Matrix(0, -self.r, 0, 1))
+        pe_r3 = self.m3 * self.g * self.y_matrix @ (self.g_w_jack @ sym.Matrix(-self.x, 0, 0, 1))
+        pe_r4 = self.m4 * self.g * self.y_matrix @ (self.g_w_jack @ sym.Matrix(0, self.y, 0, 1))
+        
+        ke_jack = ke_r1 + ke_r2 + ke_r3 + ke_r4
+        pe_jack = pe_r1 + pe_r2 + pe_r3 + pe_r4
+        
+        l_jack = ke_jack - pe_jack
+        
+        
+        
     def unhat(self):
         pass
     def inv(self):
